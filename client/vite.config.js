3d91fc9fa351a5266a38f532dee8ecfaa86d7d60
waleed <Sardarwaleed463@gmail.com>
2026-09-10 14:44:58 +0500

    Add legacy Android browser support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+    legacy({
+      targets: ['Chrome >= 64', 'Android >= 5', 'iOS >= 12', 'not IE 11'],
+    }),
   ],
- 
-  
 })
